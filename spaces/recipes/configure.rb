abf2d1007a29193d3b729462dcd429bd81195a9f
Myles Carrick <myles@mylescarrick.com>
2014-06-06 12:06:14 +1000

    spaces: couple of little template tweaks

--- spaces/recipes/configure.rb
+++ spaces/recipes/configure.rb
@@ -6,18 +6,18 @@ node[:deploy].each do |app_name, deploy_config|
   app_root = "#{deploy_config[:deploy_to]}/current"
 
   # use erb template
-  template "#{app_root}/config/settings/#{deploy_config[:rails_env]}.yml" do
+  env_template_path = "#{app_root}/config/settings/#{deploy_config[:rails_env]}.yml"
+  template env_template_path do
     source "env_settings.yml.erb"
-    cookbook "spaces"
 
     # set mode, group and owner of generated file
-    mode "0660"
+    mode "0644"
     group deploy_config[:group]
     owner deploy_config[:user]
 
     # define variable “@redis” to be used in the ERB template
     variables(
-      :redis => deploy_config[:redis] || {}
+      :redis => deploy_config[:redis]
     )
 
     # only generate a file if there is Redis configuration
