d79ad73abd1470cdaf26163ba45b2a0b9acf659c
Myles Carrick <myles@mylescarrick.com>
2014-06-06 11:55:44 +1000

    spaces: metadata and encoding note

---
+++ spaces/metadata.rb
@@ -0,0 +1,7 @@
+name        "spaces"
+description "Set up and deploy Spaces"
+maintainer  "Newington College"
+license     "MIT"
+version     "1.0.0"
+
+recipe "spaces::configure", "Basic configuration for Spaces"
