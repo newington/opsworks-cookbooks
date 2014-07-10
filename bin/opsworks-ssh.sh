7b754f2839a5dee7f123b7f47b858a109e9d6002
Harrison Conlin <me@harrisony.com>
2014-07-10 14:00:49 +1000

    be lax with ssh known hosts

--- bin/opsworks-ssh.sh
+++ bin/opsworks-ssh.sh
@@ -3,5 +3,5 @@ export AWS_DEFAULT_REGION=us-east-1
 STACK=$(aws opsworks describe-stacks | jq -r ".Stacks | map(select(.Name == \"$2\"))[0]")
 STACK_ID=$(echo $STACK | jq -r ".StackId")
 IP=$(aws opsworks describe-instances --stack-id $STACK_ID | jq -r '[.Instances[0].ElasticIp?, .Instances[0].PublicIp?, .Instances[0].PrivateIp?] | select(.)[0]')
-ssh -l $1 $IP
+ssh  -o UserKnownHostsFile='/dev/null' -o StrictHostKeyChecking=no -l $1 $IP
 
