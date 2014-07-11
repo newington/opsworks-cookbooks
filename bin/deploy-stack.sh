856119de27b285d5091dad99162ae6a3e78ffc02
Harrison Conlin <me@harrisony.com>
2014-07-11 11:05:47 +1000

    provide updates on opsworks operations

--- bin/deploy-stack.sh
+++ bin/deploy-stack.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 export AWS_DEFAULT_REGION=us-east-1
 STACK=$(aws opsworks describe-stacks | jq -r ".Stacks | map(select(.Name == \"$1\"))[0]")
 STACK_ID=$(echo $STACK | jq -r ".StackId")
 APP_ID=$(aws opsworks describe-apps --stack-id $STACK_ID | jq -r '.[][0].AppId')
 printf "Deploying: "
 aws opsworks describe-apps --stack-id $STACK_ID | jq -r '.[][0].Name'
-aws opsworks create-deployment --stack-id $STACK_ID --app-id $APP_ID --command '{"Name": "deploy"}'
+DEPLOY_ID=$(aws opsworks create-deployment --stack-id $STACK_ID --app-id $APP_ID --command '{"Name": "deploy"}' | jq -r '.DeploymentId')
 
+DEPLOY_STATUS=$(aws opsworks describe-deployments --deployment-id $DEPLOY_ID | jq -r '.Deployments[0].Status')
+while [ $DEPLOY_STATUS = "running" ]
+do
+  echo $DEPLOY_STATUS
+  sleep 2
+  DEPLOY_STATUS=$(aws opsworks describe-deployments --deployment-id $DEPLOY_ID | jq -r '.Deployments[0].Status')
+done
+echo $DEPLOY_STATUS
--- bin/update-stack-custom-cookbooks.sh
+++ bin/update-stack-custom-cookbooks.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 export AWS_DEFAULT_REGION=us-east-1
-STACK=$(aws opsworks describe-stacks | jq -r ".Stacks | map(select(.Name == \"$1\"))[0]".StackId)
-aws opsworks create-deployment --stack-id $STACK --command '{"Name": "update_custom_cookbooks"}'
+STACK_ID=$(aws opsworks describe-stacks | jq -r ".Stacks | map(select(.Name == \"$1\"))[0]".StackId)
+DEPLOY_ID=$(aws opsworks create-deployment --stack-id $STACK_ID --command '{"Name": "update_custom_cookbooks"}' | jq -r '.DeploymentId')
+
+DEPLOY_STATUS=$(aws opsworks describe-deployments --deployment-id $DEPLOY_ID | jq -r '.Deployments[0].Status')
+while [ $DEPLOY_STATUS = "running" ]
+do
+  echo $DEPLOY_STATUS
+  sleep 2
+  DEPLOY_STATUS=$(aws opsworks describe-deployments --deployment-id $DEPLOY_ID | jq -r '.Deployments[0].Status')
+done
+echo $DEPLOY_STATUS
