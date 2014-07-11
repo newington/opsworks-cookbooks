fd2c2c14397a7e686d410508e357287e1ea53afe
Harrison Conlin <me@harrisony.com>
2014-07-11 10:35:50 +1000

    correctly return ip addresses for stack

--- bin/opsworks-ssh.sh
+++ bin/opsworks-ssh.sh
@@ -2,6 +2,6 @@
 export AWS_DEFAULT_REGION=us-east-1
 STACK=$(aws opsworks describe-stacks | jq -r ".Stacks | map(select(.Name == \"$2\"))[0]")
 STACK_ID=$(echo $STACK | jq -r ".StackId")
-IP=$(aws opsworks describe-instances --stack-id $STACK_ID | jq -r '[.Instances[0].ElasticIp?, .Instances[0].PublicIp?, .Instances[0].PrivateIp?] | select(.)[0]')
+IP=$(aws opsworks describe-instances --stack-id $STACK_ID | jq -r '[.Instances[0].ElasticIp?, .Instances[0].PublicIp?, .Instances[0].PrivateIp?] | map(select(.))[0]')
 ssh  -o UserKnownHostsFile='/dev/null' -o StrictHostKeyChecking=no -l $1 $IP
 
