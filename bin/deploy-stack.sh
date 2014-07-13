59c18c4c366fa486b43de2ea77ed41963e3713d8
Harrison Conlin <me@harrisony.com>
2014-07-14 08:10:14 +1000

    migrate when deploying

--- bin/deploy-stack.sh
+++ bin/deploy-stack.sh
@@ -3,9 +3,11 @@ export AWS_DEFAULT_REGION=us-east-1
 STACK=$(aws opsworks describe-stacks | jq -r ".Stacks | map(select(.Name == \"$1\"))[0]")
 STACK_ID=$(echo $STACK | jq -r ".StackId")
 APP_ID=$(aws opsworks describe-apps --stack-id $STACK_ID | jq -r '.[][0].AppId')
+
 printf "Deploying: "
 aws opsworks describe-apps --stack-id $STACK_ID | jq -r '.[][0].Name'
-DEPLOY_ID=$(aws opsworks create-deployment --stack-id $STACK_ID --app-id $APP_ID --command '{"Name": "deploy"}' | jq -r '.DeploymentId')
+
+DEPLOY_ID=$(aws opsworks create-deployment --stack-id $STACK_ID --app-id $APP_ID --command '{"Name":"deploy","Args":{"migrate":["true"]}}' | jq -r '.DeploymentId')
 
 DEPLOY_STATUS=$(aws opsworks describe-deployments --deployment-id $DEPLOY_ID | jq -r '.Deployments[0].Status')
 while [ $DEPLOY_STATUS = "running" ]
